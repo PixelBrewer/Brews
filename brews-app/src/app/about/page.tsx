28b37e82be455b1e7e9e461093c2d13d6d0dca82
PixelBrewer <pixelbrewerstudios@gmail.com>
2024-10-20 22:38:03 -0700

    Fixing copy errors

--- brews-app/src/app/about/page.tsx
+++ brews-app/src/app/about/page.tsx
@@ -15,12 +15,12 @@ export default function About() {
         <div className='flex justify-center'>
           <p className='text-2xl p-4 m-2 max-w-2xl text-justify'>
             Have you ever been in a situation where you buy some coffee beans
-            from a local shop to try. You take them home, make your favorite
+            from a local shop to try? You take them home, make your favorite
             kind of coffee, then you realize that the coffee is one of your
             favorites. You want to record this type of bean, the roast, and
             where you bought these beans just in case you want to return to
             somthing familiar and lovely. But writing that down on a piece of
-            paper seems a bit archaic. Well, you are in luck! Taht is where
+            paper seems a bit archaic. Well, you are in luck! That is where
             Brews comes in. This app is aimed at helping you create a catalogue
             of your favorite coffee beans. You can add the name of the bean, the
             roast, the shop you bought it from, and even give it a rating. You
