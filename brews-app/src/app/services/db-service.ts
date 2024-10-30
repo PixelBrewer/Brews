1c315a75f5a67a5897ecacef5bf0fc89664941ed
AlexJBlair <alex_blair42@outlook.com>
2024-10-30 00:13:48 -0700

    Post Put and Delete are a work in progress

--- brews-app/src/app/services/db-service.ts
+++ brews-app/src/app/services/db-service.ts
@@ -54,112 +54,112 @@ export const getSingleBrew = async (name: string) => {
   }
 };
 
-export const addNewBrew = async (brew: any) => {
-  try {
-    const newBrew = await prisma.coffee_beans.create({
-      data: brew,
-    });
-    return newBrew;
-  } catch (error) {
-    throw new Error(`Error adding new brew: ${error}`);
-  }
-};
+// export const addNewBrew = async (brew: any) => {
+//   try {
+//     const newBrew = await prisma.coffee_beans.create({
+//       data: brew,
+//     });
+//     return newBrew;
+//   } catch (error) {
+//     throw new Error(`Error adding new brew: ${error}`);
+//   }
+// };
 
-export const addPurchaseInfo = async (purchase: any) => {
-  try {
-    const newPurchase = await prisma.purchases.create({
-      data: purchase,
-    });
-    return newPurchase;
-  } catch (error) {
-    throw new Error(`Error adding purchase info: ${error}`);
-  }
-};
+// export const addPurchaseInfo = async (purchase: any) => {
+//   try {
+//     const newPurchase = await prisma.purchases.create({
+//       data: purchase,
+//     });
+//     return newPurchase;
+//   } catch (error) {
+//     throw new Error(`Error adding purchase info: ${error}`);
+//   }
+// };
 
-export const addRating = async (rating: any) => {
-  try {
-    const newRating = await prisma.ratings.create({
-      data: rating,
-    });
-    return newRating;
-  } catch (error) {
-    throw new Error(`Error adding rating: ${error}`);
-  }
-};
+// export const addRating = async (rating: any) => {
+//   try {
+//     const newRating = await prisma.ratings.create({
+//       data: rating,
+//     });
+//     return newRating;
+//   } catch (error) {
+//     throw new Error(`Error adding rating: ${error}`);
+//   }
+// };
 
-export const addManufacturer = async (manufacturer: any) => {
-  try {
-    const newManufacturer = await prisma.manufacturers.create({
-      data: manufacturer,
-    });
-    return newManufacturer;
-  } catch (error) {
-    throw new Error(`Error adding manufacturer: ${error}`);
-  }
-};
+// export const addManufacturer = async (manufacturer: any) => {
+//   try {
+//     const newManufacturer = await prisma.manufacturers.create({
+//       data: manufacturer,
+//     });
+//     return newManufacturer;
+//   } catch (error) {
+//     throw new Error(`Error adding manufacturer: ${error}`);
+//   }
+// };
 
-export const updateBrew = async (name: string, brew: any) => {
-  try {
-    const updatedBrew = await prisma.coffee_beans.update({
-      where: {
-        name: name,
-      },
-      data: brew,
-    });
-    return updatedBrew;
-  } catch (error) {
-    throw new Error(`Error updating brew: ${error}`);
-  }
-};
+// export const updateBrew = async (name: string, brew: any) => {
+//   try {
+//     const updatedBrew = await prisma.coffee_beans.update({
+//       where: {
+//         name: name,
+//       },
+//       data: brew,
+//     });
+//     return updatedBrew;
+//   } catch (error) {
+//     throw new Error(`Error updating brew: ${error}`);
+//   }
+// };
 
-export const deleteBrew = async (name: string) => {
-  try {
-    const deletedBrew = await prisma.coffee_beans.delete({
-      where: {
-        name: name,
-      },
-    });
-    return deletedBrew;
-  } catch (error) {
-    throw new Error(`Error deleting brew: ${error}`);
-  }
-};
+// export const deleteBrew = async (name: string) => {
+//   try {
+//     const deletedBrew = await prisma.coffee_beans.delete({
+//       where: {
+//         name: name,
+//       },
+//     });
+//     return deletedBrew;
+//   } catch (error) {
+//     throw new Error(`Error deleting brew: ${error}`);
+//   }
+// };
 
-export const deletePurchase = async (id: number) => {
-  try {
-    const deletedPurchase = await prisma.purchases.delete({
-      where: {
-        id: id,
-      },
-    });
-    return deletedPurchase;
-  } catch (error) {
-    throw new Error(`Error deleting purchase: ${error}`);
-  }
-};
+// export const deletePurchase = async (id: number) => {
+//   try {
+//     const deletedPurchase = await prisma.purchases.delete({
+//       where: {
+//         id: id,
+//       },
+//     });
+//     return deletedPurchase;
+//   } catch (error) {
+//     throw new Error(`Error deleting purchase: ${error}`);
+//   }
+// };
 
-export const deleteRating = async (id: number) => {
-  try {
-    const deletedRating = await prisma.ratings.delete({
-      where: {
-        id: id,
-      },
-    });
-    return deletedRating;
-  } catch (error) {
-    throw new Error(`Error deleting rating: ${error}`);
-  }
-};
+// export const deleteRating = async (id: number) => {
+//   try {
+//     const deletedRating = await prisma.ratings.delete({
+//       where: {
+//         id: id,
+//       },
+//     });
+//     return deletedRating;
+//   } catch (error) {
+//     throw new Error(`Error deleting rating: ${error}`);
+//   }
+// };
 
-export const deleteManufacturer = async (id: number) => {
-  try {
-    const deletedManufacturer = await prisma.manufacturers.delete({
-      where: {
-        id: id,
-      },
-    });
-    return deletedManufacturer;
-  } catch (error) {
-    throw new Error(`Error deleting manufacturer: ${error}`);
-  }
-};
+// export const deleteManufacturer = async (id: number) => {
+//   try {
+//     const deletedManufacturer = await prisma.manufacturers.delete({
+//       where: {
+//         id: id,
+//       },
+//     });
+//     return deletedManufacturer;
+//   } catch (error) {
+//     throw new Error(`Error deleting manufacturer: ${error}`);
+//   }
+// };
